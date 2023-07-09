4905398c3feb8cc5244c2efe433f068fcbdf6059
ec33sd <119135916+ec33sd@users.noreply.github.com>
2023-07-09 17:41:08 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 document.getElementById("header").innerHTML=` <nav class="navbar navbar-expand-sm
 navbar-light bg-transparent">
 <div class="container">
-<a class="navbar-brand" href="../TIFCAC/templates/productos.html">AMALIA CALZADOS</a>
+<a class="navbar-brand" href="../templates/productos.html">AMALIA CALZADOS</a>
 
 </div>
 </nav>`
